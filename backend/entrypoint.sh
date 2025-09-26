2dce5789601896f2606e073ad2277499bf062c44
roman <gastursolar@gmail.com>
2025-09-26 20:27:59 +0500

    change backend entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 # uv run python manage.py loaddata initial_data.json || true  # если фикстуры есть
